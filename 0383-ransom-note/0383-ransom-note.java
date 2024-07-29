9310588a39ed7d2d017f3906dbe8b061a54c58e6
HyunJin Kim <hyunnjinn@gmail.com>
2024-07-29 18:41:38 +0900

    Time: 14 ms (30.90%), Space: 44.8 MB (53.76%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,20 +1,18 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
         Map<Character, Integer> map = new HashMap<>();
+        
         for (char c : magazine.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
         
-        boolean result = true;
         for (char c : ransomNote.toCharArray()) {
-            Integer count = map.get(c);
-            if (count == null || count == 0) {
-                result = false;
-                break;
+            int count = map.getOrDefault(c, 0);
+            if (count == 0) {
+                return false;
             }
             map.put(c, count - 1);
         }
-        
-        return result;
+        return true;
     }
 }

eb18e98b4b8a9a2180d9278bba56f133952e1ef0
HyunJin Kim <hyunnjinn@gmail.com>
2024-07-30 22:17:41 +0900

    Time: 1 ms, Memory: 45.1 MB - LeetHub

--- valid-mountain-array/valid-mountain-array.java
+++ valid-mountain-array/valid-mountain-array.java
@@ -1,35 +1,24 @@
 class Solution {
     public boolean validMountainArray(int[] arr) {
-        if (arr.length <= 2) {
+        if (arr.length < 3) {
             return false;
         }
         
-        int top = 0;
-        for (int i = 0; i < arr.length; i++) {
-            top = Math.max(top, arr[i]);
+        int index = 0;
+        int end = arr.length - 1;
+        
+        while (index < end && arr[index] < arr[index + 1]) {
+            index++;
         }
         
-        boolean isTop = false;
-        for (int i = 0; i < arr.length - 1; i++) {
-            if (arr[i] == arr[i + 1]) {
-                return false;
-            }
-            
-            if (i != 0 && arr[i] == top) {
-                isTop = true;
-            }
-            
-            if (arr[i] < arr[i + 1]) {
-                if (isTop) {
-                    return false;
-                }
-            } else if (arr[i] > arr[i + 1]) {
-                if (!isTop) {
-                    return false;
-                }
-            }
+        if (index == 0 || index == end) {
+            return false;
         }
         
-        return isTop;
+        while (index < end && arr[index] > arr[index + 1]) {
+            index++;
+        }
+        
+        return index == end;
     }
 }

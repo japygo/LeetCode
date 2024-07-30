27d23a632b96f54815e23d8b80993607ac629d2a
HyunJin Kim <hyunnjinn@gmail.com>
2024-07-30 22:12:43 +0900

    Time: 3 ms, Memory: 42.7 MB - LeetHub

--- check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
+++ check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean checkIfExist(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] == 2 * arr[j] || arr[j] == 2 * arr[i]) {
-                    return true;
-                }
+        Set<Integer> set = new HashSet<>();
+        for (int num : arr) {
+            if (set.contains(2 * num) || (num % 2 == 0 && set.contains(num / 2))) {
+                return true;
             }
+            set.add(num);
         }
         return false;
     }

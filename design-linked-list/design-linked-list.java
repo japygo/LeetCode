a92b2335b2fccf63d8ed17cc873bbf0387d71eb3
HyunJin Kim <hyunnjinn@gmail.com>
2024-08-04 15:37:25 +0900

    Time: 6 ms, Memory: 45.2 MB - LeetHub

--- design-linked-list/design-linked-list.java
+++ design-linked-list/design-linked-list.java
@@ -1,47 +1,54 @@
 class MyLinkedList {
-    
     private Node head;
+    private Node tail;
     private int size;
-
+    
     public MyLinkedList() {
         this.head = null;
+        this.tail = null;
         this.size = 0;
     }
     
     public int get(int index) {
-        if (index >= size) {
+        if (index < 0 || index >= size) {
             return -1;
         }
         
-        Node node = head;
-        while (index-- > 0) {
-            node = node.next;
-        }
+        Node node = getNode(index);
+        
         return node.val;
     }
     
     public void addAtHead(int val) {
-        Node node = new Node(val);
-        node.next = head;
-        this.head = node;
-        this.size++;
+        Node newNode = new Node(val);
+        
+        newNode.next = head;
+        if (head != null) {
+            head.prev = newNode;
+        } else {
+            tail = newNode;
+        }
+        head = newNode;
+        
+        size++;
     }
     
     public void addAtTail(int val) {
-        if (head == null) {
-            this.head = new Node(val);
+        Node newNode = new Node(val);
+        
+        if (tail == null) {
+            head = newNode;
         } else {
-            Node node = head;
-            while(node.next != null) {
-                node = node.next;
-            }
-            node.next = new Node(val);
+            tail.next = newNode;
+            newNode.prev = tail;
         }
-        this.size++;
+        tail = newNode;
+        
+        size++;
     }
     
     public void addAtIndex(int index, int val) {
-        if (index > size) {
+        if (index < 0 || index > size) {
             return;
         }
         if (index == 0) {
@@ -53,43 +60,78 @@ class MyLinkedList {
             return;
         }
         
-        Node node = head;
-        while (index-- > 1) {
-            node = node.next;
-        }
+        Node prevNode = getNode(index - 1);
         Node newNode = new Node(val);
-        newNode.next = node.next;
-        node.next = newNode;
-        this.size++;
+        Node nextNode = prevNode.next;
+        
+        newNode.prev = prevNode;
+        newNode.next = nextNode;
+        prevNode.next = newNode;
+        if (nextNode != null) {
+            nextNode.prev = newNode;
+        }
+        
+        size++;
     }
     
     public void deleteAtIndex(int index) {
-        if (index >= size) {
+        if (index < 0 || index >= size) {
             return;
         }
+        
         if (index == 0) {
             head = head.next;
+            if (head != null) {
+                head.prev = null;
+            } else {
+                tail = null;
+            }
+        } else if (index == size - 1) {
+            tail = tail.prev;
+            if (tail != null) {
+                tail.next = null;
+            } else {
+                head = null;
+            }
         } else {
-            Node node = head;
-            while (index-- > 1) {
+            Node node = getNode(index);
+            Node prevNode = node.prev;
+            Node nextNode = node.next;
+            
+            if (prevNode != null) {
+                prevNode.next = nextNode;
+            }
+            if (nextNode != null) {
+                nextNode.prev = prevNode;
+            }
+        }
+        
+        size--;
+    }
+    
+    private Node getNode(int index) {
+        Node node;
+        if (index < size / 2) {
+            node = head;
+            for (int i = 0; i < index; i++) {
                 node = node.next;
             }
-            node.next = node.next.next;
+        } else {
+            node = tail;
+            for (int i = size - 1; i > index; i--) {
+                node = node.prev;
+            }
         }
-        this.size--;
+        return node;
     }
     
     private class Node {
         private int val;
         private Node next;
-        
-        public Node(int val, Node next) {
-            this.val = val;
-            this.next = next;
-        }
-        
+        private Node prev;
+                
         public Node(int val) {
-            this(val, null);
+            this.val = val;
         }
     }
 }

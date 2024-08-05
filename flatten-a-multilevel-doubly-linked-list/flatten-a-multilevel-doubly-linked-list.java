0ec6382e2c4d67ed3333a41b4ccdd31f61da6148
HyunJin Kim <hyunnjinn@gmail.com>
2024-08-05 17:40:18 +0900

    Time: 0 ms, Memory: 41.8 MB - LeetHub

---
+++ flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.java
@@ -0,0 +1,39 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+};
+*/
+
+class Solution {
+    public Node flatten(Node head) {
+        Node node = head;
+        
+        while (node != null) {
+            if (node.child != null) {
+                Node child = node.child;
+                Node tmp = node.next;
+                
+                node.next = child;
+                child.prev = node;
+                node.child = null;
+                
+                while (child.next != null) {
+                    child = child.next;
+                }
+                
+                if (tmp != null) {
+                    child.next = tmp;
+                    tmp.prev = child;
+                }
+            }
+            
+            node = node.next;
+        }
+        
+        return head;
+    }
+}

eabf2ab4da740f82c930d68e2fe322c18bc1f0bb
HyunJin Kim <hyunnjinn@gmail.com>
2025-05-13 22:49:50 +0900

    Time: 1 ms (82.18%), Space: 41.9 MB (45.25%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < word1.length(); i++) {
+            sb.append(word1.charAt(i));
+            if (i < word2.length()) {
+                sb.append(word2.charAt(i));
+            }
+        }
+
+        if (word1.length() < word2.length()) {
+            sb.append(word2.substring(word1.length()));
+        }
+
+        return sb.toString();
+    }
+}

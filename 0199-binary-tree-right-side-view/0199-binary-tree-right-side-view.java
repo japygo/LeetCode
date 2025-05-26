96a4bee92403989bcc5bb455ad7dd0afb7204234
HyunJin Kim <hyunnjinn@gmail.com>
2025-05-26 09:59:06 +0900

    Time: 1 ms (71.81%), Space: 42 MB (80.01%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -15,31 +15,29 @@
  */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        Integer[] result = new Integer[101];
         if (root == null) {
             return List.of();
         }
 
-        Queue<Object[]> queue = new LinkedList<>();
-        queue.add(new Object[]{root, 0});
+        List<Integer> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
 
         while (!queue.isEmpty()) {
-            Object[] poll = queue.poll();
-            TreeNode node = (TreeNode) poll[0];
-            int depth = (int) poll[1];
-
-            result[depth] = node.val;
-
-            if (node.left != null) {
-                queue.add(new Object[]{node.left, depth + 1});
+            int size = queue.size();
+            TreeNode node = null;
+            for (int i = 0; i < size; i++) {
+                node = queue.poll();
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
             }
-            if (node.right != null) {
-                queue.add(new Object[]{node.right, depth + 1});
-            }
+            result.add(node.val);
         }
 
-        return Arrays.stream(result)
-                     .filter(Objects::nonNull)
-                     .toList();
+        return result;
     }
 }

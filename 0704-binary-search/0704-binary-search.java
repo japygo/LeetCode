749288c8467f43b44511821c09e23a491d4d0f79
HyunJin Kim <hyunnjinn@gmail.com>
2025-08-01 08:29:10 +0900

    Time: 0 ms (100%), Space: 46.1 MB (22.91%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int num = nums[mid];
+            if (num == target) {
+                return mid;
+            } else if (num < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
